c7beb0fa2f4886ec8ea1e844e80e211d95729822
diego-acc <diego.sm.acc@gmail.com>
2021-03-30 23:29:27 +0200

    server & cli work

--- cliente.py
+++ cliente.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import signal, select, socket, sys, os                                      #Funciones orientadas a conexión, sistema
+import signal, queue, select, socket, sys, os                                      #Funciones orientadas a conexión, sistema
 from time import time                                                       #Cronometrar tiempos
 from time import sleep
 
 ERR = "\033[93m"
 END = "\033[0m"
+BAR = "\n———————————————————————————————\n"
 
 if __name__ == "__main__":
     if len(sys.argv) != 3 :                                                 #Esta función compara los tiempos para
@@ -16,21 +17,64 @@ if __name__ == "__main__":
     #Variables socket
     servIP = sys.argv[1]
     servPort = int(sys.argv[2])
-    server_address = (servIP, servPort)
+    serv_addr = (servIP, servPort)
     
     if servPort < 1023:                                                     #Comprobamos el nº de puerto
         print(ERR + "ERR: El nº de puerto debe ser mayor que 1023" + END)
         sys.exit()
 
+    name = ""
+    while len(name) <= 0:
+        name = input("Nombre: ")
+
     #Creación del socket UDP
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     #Conexión socket
-    sock.connect(server_address)
+
+    print(BAR, "Conectando con %s:%s" % serv_addr, BAR)
+
+    sock.connect(serv_addr)
+
+    inputs = [sys.stdin, sock]
+    outputs = [sock]
+    que = queue.Queue()
+
+    mssNext = str.encode("")
 
     while True:
         try:
-            mss = input("Mensaje: ")
-            sock.send(mss.encode())
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+
+            for sck in readable:
+                if sck == sys.stdin:
+                    mssIn = sys.stdin.readline()
+                    mss = "->" + name + ": " + mssIn
+                    que.put(mss.encode())
+
+                elif sck == sock:
+                    dataRecv = sock.recv(1024)
+                    if dataRecv:
+                        if dataRecv.decode("utf-8") != mssNext.decode("utf-8").strip("\n"):
+                            print(dataRecv.decode("utf-8"))
+                    else:
+                        sock.close()
+                        print("Conexión finalizada")
+                        sys.exit(0)
+
+            for sck in writable:
+                if sck == sock:
+                    try:
+                        mssNext = que.get_nowait()
+                        sock.send(mssNext)
+                    
+                    except queue.Empty:
+                        pass
+
+            for sck in exceptional:
+                if sck == sock:
+                    sock.close()
+
         except KeyboardInterrupt:
+            sock.close()
             sys.exit()
--- servidor.py
+++ servidor.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import signal, select, socket, sys, os                                      #Funciones orientadas a conexión, sistema
-from time import time                                                       #Cronometrar tiempos
+import signal, select, socket, sys, os, re                                      #Funciones orientadas a conexión, sistema
+from time import time                                                           #Cronometrar tiempos
 from time import sleep
 
 ERR = "\033[93m"
 END = "\033[0m"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 :                                                 #Esta función compara los tiempos para
+    if len(sys.argv) != 2 :                                                     #Esta función compara los tiempos para
         print(ERR + "ERR: Nº de argumentos no válidos" + END)
         sys.exit()
     
@@ -18,41 +18,67 @@ if __name__ == "__main__":
     servPort = int(sys.argv[1])
     serv_addr = (servIP, servPort)
 
-    if servPort < 1023:                                                     #Comprobamos el nº de puerto
+    if servPort < 1023:                                                         #Comprobamos el nº de puerto
         print(ERR + "ERR: El nº de puerto debe ser mayor que 1023" + END)
         sys.exit()
 
     #Creación del socket TCP
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(0)                                                         #Establecemos modo no bloqueo
 
     #Conexión socket
     sock.bind(serv_addr)
 
-    sock.listen(1)                                                          #El socket está a la escucha del cliente
-    listCli = [sock]                                                        #Lista con un solo socket como elemento
+    sock.listen(10)                                                             #El socket está a la escucha del cliente
+    inputs = [sock]                                                             #Lista de sockets de lectura
+    outputs = []                                                                #Lista de sockets de escritura
+    clientes = {}                                                               #Lista de clientes  
+    nCli = 0
 
     while True:
         try:
-            readCli, writeCli, exctCli = select.select(listCli, [], ())     #Select bloqueado con listCli
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)     #Select coordina entre i/o
 
-            for sck in readCli:
-                if sck == sock:
-                    cli, cli_add = sock.accept()
+            for sck in readable:
+                if sck == sock:                                         
+                    cli, cli_add = sock.accept()                                #Aceptamos la conexión de un socket "readable"
+                    cli.setblocking(0)                                          #Establecemos modo no bloqueo
                     print("-Cliente:", cli_add)
-                    listCli.append(cli)
+
+                    inputs.append(cli)                                          #Añadimos a la lista inputs
+                    outputs.append(cli)                                         #Añadimos a la lista outputs
+
+                    clientes[cli] = "cli" + str(nCli)
+                    cliName = clientes[cli]
+                    nCli += 1
+
+                    for s in outputs:
+                        mss = "->Server: Nuevo usuario" + str(cli_add)
+                        s.send(mss.encode())                                    #Avisamos del nuevo usuario
 
                 else:
-                    dataRecv = sck.recvfrom(1024)
-                    mss = dataRecv[0].decode()
-                    lenMss = len(mss)
+                    dataRecv = sck.recv(1024)
+                    if dataRecv:
+                        dataRecv = dataRecv.strip().decode('utf-8')
+                        print(dataRecv)
+                        for s in outputs:
+                            s.send(dataRecv.encode())                           #Renviamos mensaje a todos los clientes
 
-                    if lenMss > 0:
-                        print("cli: ", mss)
-                    
                     else:
-                        print("-Cliente", cli_add, "desconectado")
+                        outputs.remove(sck)
+                        inputs.remove(sck)
                         sck.close()
-                        listCli.remove(sck)
+
+                        for s in outputs:
+                            mss = "->Server: " + str(cli_add) + " desconectado"
+                            s.send(mss.encode())                                #Avisamos de usuario desconectado
+
+
+            for sck in exceptional:                                             #Condiciones excepcionales
+                inputs.remove(sck)                                              #Eliminamos el socket de la lista de inputs
+                outputs.remove(sck)                                             #Eliminamos el socket de la lista de outputs
+                sck.close()
         
         except KeyboardInterrupt:
+            print("\nCerrando conexión con clientes")
             sys.exit()
