92e849adf7e30769b12a272b975e2f60d5061dd8
mariadm3 <maria.dominguezv@edu.uah.es>
2021-04-06 22:18:46 +0200

    cliente v2.0 (comentarios)

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import signal, queue, select, socket, sys, os                                      #Funciones orientadas a conexión, sistema
+import signal, queue, select, socket, sys, os                               #Funciones orientadas a conexión, sistema, json y expresiones regulares
 from time import time                                                       #Cronometrar tiempos
 from time import sleep
 
@@ -10,7 +10,7 @@ END = "\033[0m"
 BAR = "\n———————————————————————————————\n"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3 :                                                 #Esta función compara los tiempos para
+    if len(sys.argv) != 3 :                                                 
         print(ERR + "ERR: Nº de argumentos no válidos" + END)
         sys.exit()
 
@@ -31,49 +31,48 @@ if __name__ == "__main__":
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     #Conexión socket
-
     print(BAR, "Conectando con %s:%s" % serv_addr, BAR)
 
-    sock.connect(serv_addr)
+    sock.connect(serv_addr)                                                 #Establecemos conexión con el servidor
 
-    inputs = [sys.stdin, sock]
-    outputs = [sock]
-    que = queue.Queue()
+    inputs = [sys.stdin, sock]                                              #Lista de sockets de entrada
+    outputs = [sock]                                                        #Lista de sockets de salida
+    que = queue.Queue()                                                     #Creamos una cola
 
     mssNext = str.encode("")
 
     while True:
         try:
-            readable, writable, exceptional = select.select(inputs, outputs, inputs)
+            readable, writable, exceptional = select.select(inputs, outputs, inputs)     #Select coordina entre i/o
 
-            for sck in readable:
-                if sck == sys.stdin:
+            for sck in readable:                                            #Sockets de entrada disponibles
+                if sck == sys.stdin:                                        #Si detecta el teclado
                     mssIn = sys.stdin.readline()
                     mss = "->" + name + ": " + mssIn
                     que.put(mss.encode())
 
-                elif sck == sock:
+                elif sck == sock:                                           #Un nuevo socket desea conectarse
                     dataRecv = sock.recv(1024)
                     if dataRecv:
-                        if dataRecv.decode("utf-8") != mssNext.decode("utf-8").strip("\n"):
+                        if dataRecv.decode("utf-8") != mssNext.decode("utf-8").strip("\n"): 
                             print(dataRecv.decode("utf-8"))
                     else:
-                        sock.close()
+                        sock.close()                                        #Cerramos el socket
                         print("Conexión finalizada")
                         sys.exit(0)
 
-            for sck in writable:
-                if sck == sock:
+            for sck in writable:                                            #Sockets de salida disponibles
+                if sck == sock:                                             #Un nuevo socket desea
                     try:
-                        mssNext = que.get_nowait()
+                        mssNext = que.get_nowait()                          #Devuelve la línea capturada en sys.stdin
                         sock.send(mssNext)
                     
                     except queue.Empty:
                         pass
 
-            for sck in exceptional:
+            for sck in exceptional:                                         #Condiciones excepcionales
                 if sck == sock:
-                    sock.close()
+                    sock.close()                                            #Cerramos el socket
 
         except KeyboardInterrupt:
             sock.close()
